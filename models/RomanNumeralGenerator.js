31085aae50ceefb38a226ceb70741b0aea728bb3
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-07-16 17:50:04 +0100

    Add initial acceptance criteria

--- models/RomanNumeralGenerator.js
+++ models/RomanNumeralGenerator.js
@@ -1,5 +1,8 @@
 class RomanNumeralGenerator {
 
+  generate(number) {
+
+  }
 }
 
 module.exports = RomanNumeralGenerator;
--- models/specs/RomanNumeralGenerator_spec.js
+++ models/specs/RomanNumeralGenerator_spec.js
@@ -9,7 +9,31 @@ describe('Roman Numeral Generator', function() {
     generator = new RomanNumeralGenerator();
   })
 
-  it('should be tested', function() {
-    assert.strictEqual('test', 'test')
+  xit('should return 1 as I', function() {
+    assert.strictEqual(generator.generate(1), 'I')
+  })
+
+  xit('should return 5 as V', function() {
+    assert.strictEqual(generator.generate(5), 'V')
+  })
+
+  xit('should return 10 as X', function() {
+    assert.strictEqual(generator.generate(4), 'X')
+  })
+
+  xit('should return 50 as L', function() {
+    assert.strictEqual(generator.generate(50), 'L')
+  })
+
+  xit('should return 100 as C', function() {
+    assert.strictEqual(generator.generate(100), 'C')
+  })
+
+  xit('should return 500 as D', function() {
+    assert.strictEqual(generator.generate(4), 'D')
+  })
+
+  xit('should return 1000 as M', function() {
+    assert.strictEqual(generator.generate(4), 'M')
   })
 })
