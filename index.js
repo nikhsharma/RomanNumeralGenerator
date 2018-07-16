deea0783126a71eeb31e4a7e95cedef890989dad
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-07-16 22:31:11 +0100

    Add white space to index.js for readability

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const generator = new RomanNumeralGenerator();
 let input;
 
 while (input !== 'q') {
-input = conif.getConsoleInput('Input a number between 1 and 3999 generate its roman numeral equivilant. (enter q to quit):   ');
-if (input >= 1 || input <= 3999) {
-  console.log(generator.generate(input));
-} else if (input === 'q') {
-  console.log('Goodbye');
-} else {
-  console.log('please input a number between 1 - 3999. (enter 0 to quit)');
-}
+
+  input = conif.getConsoleInput('Input a number between 1 and 3999 generate its roman numeral equivilant. (enter q to quit):   ');
+  
+  if (input >= 1 || input <= 3999) {
+    console.log(generator.generate(input));
+  } else if (input === 'q') {
+    console.log('Goodbye');
+  } else {
+    console.log('please input a number between 1 - 3999. (enter 0 to quit)');
+  }
 }
