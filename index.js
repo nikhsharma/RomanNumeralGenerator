b309475244ea47c3ded8a69fa268c88d34ea12d4
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-07-17 23:22:16 +0100

    Minor adjustment to text in index

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ let input;
 while (input !== 'q') {
 
   // Get user input
-  input = conif.getConsoleInput('Input a number between 1 and 3999 to generate its roman numeral equivilant. (enter q to quit):   ');
+  input = conif.getConsoleInput('Input a number between 1-3999 to generate its roman numeral equivilant. (q to quit):   ');
 
   // Check user input
   if (input >= 1 && input <= 3999) {
-    console.log(romanNumeralGenerator.generate(input));
+    console.log(input, 'in roman numerals is:', romanNumeralGenerator.generate(input));
   } else if (input === 'q') {
     console.log('Goodbye');
   } else {
