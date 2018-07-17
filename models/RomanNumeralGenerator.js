1572464ee8d7c7fba8a34e059d2d1f70db63dfed
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-07-17 22:58:20 +0100

    Slight reformat for readability

--- models/RomanNumeralGenerator.js
+++ models/RomanNumeralGenerator.js
@@ -9,7 +9,7 @@ class RomanNumeralGenerator {
       500: 'D',
       1000: 'M'
     }
-  }
+  };
 
   generate(number) {
     // If invalid number, return null
@@ -21,19 +21,19 @@ class RomanNumeralGenerator {
     let result;
 
     // Seperate number's digits
-    const singleDigits = number % 10
-    number -= singleDigits
-    const tens = number % 100
-    number -= tens
-    const hundreds = number % 1000
-    number -= hundreds
-    const thousands = number
+    const singleDigits = number % 10;
+    number -= singleDigits;
+    const tens = number % 100;
+    number -= tens;
+    const hundreds = number % 1000;
+    number -= hundreds;
+    const thousands = number;
 
     // Generate symbol for each digit seperately, add it to result string
-    result = this.generateIndividualSymbol(thousands)
-    result += this.generateIndividualSymbol(hundreds)
-    result += this.generateIndividualSymbol(tens)
-    result += this.generateIndividualSymbol(singleDigits)
+    result = this.generateIndividualSymbol(thousands);
+    result += this.generateIndividualSymbol(hundreds);
+    result += this.generateIndividualSymbol(tens);
+    result += this.generateIndividualSymbol(singleDigits);
 
     return result;
   }
@@ -41,8 +41,8 @@ class RomanNumeralGenerator {
 
   generateIndividualSymbol(number) {
     // Create seperate arrays for the symbols and their values in descending order
-    const symbols = Object.values(this.symbolsList).reverse()
-    const values = Object.keys(this.symbolsList).reverse()
+    const symbols = Object.values(this.symbolsList).reverse();
+    const values = Object.keys(this.symbolsList).reverse();
 
     let symbolResult = '';
 
@@ -54,31 +54,30 @@ class RomanNumeralGenerator {
       for (let value of values) {
         if (number >= value) {
           // Add symbol to result, subtract the symbol's value from number, reset the counter, restart loop.
-          symbolResult += symbols[counter]
-          number -= value
+          symbolResult += symbols[counter];
+          number -= value;
           counter = 0;
-          break
+          break;
         } else if (number === (value - values[counter+2])) {
           // Check if symbol should be preceded by second smaller symbol
-          symbolResult += (symbols[counter+2] + symbols[counter])
-          number -= (value - values[counter+2])
+          symbolResult += (symbols[counter+2] + symbols[counter]);
+          number -= (value - values[counter+2]);
           counter = 0;
-          break
+          break;
         } else if (number === (value - values[counter+1])) {
           // Check if symbol should be preceded by next smaller symbol
-          symbolResult += (symbols[counter+1] + symbols[counter])
-          number -= (value - values[counter+1])
+          symbolResult += (symbols[counter+1] + symbols[counter]);
+          number -= (value - values[counter+1]);
           counter = 0;
-          break
+          break;
         } else {
           // Increment counter if current symbol not needed
-          counter++
+          counter++;
         }
       }
     }
     return symbolResult;
+  };
+};
 
-  }
-}
-
-  module.exports = RomanNumeralGenerator;
+module.exports = RomanNumeralGenerator;
