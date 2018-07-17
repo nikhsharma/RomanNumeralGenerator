2d7d64d6a0a5d2ade1e694ac0eed144850dc3164
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-07-17 22:37:37 +0100

    Add comments for clarity

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ let input;
 
 while (input !== 'q') {
 
-  input = conif.getConsoleInput('Input a number between 1 and 3999 generate its roman numeral equivilant. (enter q to quit):   ');
-  
-  if (input >= 1 || input <= 3999) {
+  input = conif.getConsoleInput('Input a number between 1 and 3999 to generate its roman numeral equivilant. (enter q to quit):   ');
+
+  if (input >= 1 && input <= 3999) {
     console.log(generator.generate(input));
   } else if (input === 'q') {
     console.log('Goodbye');
   } else {
-    console.log('please input a number between 1 - 3999. (enter 0 to quit)');
+    console.log('please input a number between 1 - 3999.');
   }
 }
--- models/RomanNumeralGenerator.js
+++ models/RomanNumeralGenerator.js
@@ -12,59 +12,71 @@ class RomanNumeralGenerator {
   }
 
   generate(number) {
+    // If invalid number, return null
     if (number < 1 || number > 3999) return null;
+
+    // If number is in the symbol list, return it's symbol
     if (this.symbolsList[number]) return this.symbolsList[number];
 
+    let result;
+
+    // Seperate number's digits
     const singleDigits = number % 10
     number -= singleDigits
-
     const tens = number % 100
     number -= tens
-
     const hundreds = number % 1000
     number -= hundreds
-
     const thousands = number
 
-    let result = this.generateIndividualSymbol(thousands)
+    // Generate symbol for each digit seperately, add it to result string
+    result = this.generateIndividualSymbol(thousands)
     result += this.generateIndividualSymbol(hundreds)
     result += this.generateIndividualSymbol(tens)
     result += this.generateIndividualSymbol(singleDigits)
 
     return result;
-
   }
 
+
   generateIndividualSymbol(number) {
-    const values = [1000, 500, 100, 50, 10, 5, 1]
-    const symbols = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
-    let symbol = '';
+    // Create seperate arrays for the symbols and their values in descending order
+    const symbols = Object.values(this.symbolsList).reverse()
+    const values = Object.keys(this.symbolsList).reverse()
+
+    let symbolResult = '';
 
     while (number !== 0) {
 
+      // Counter used for index position in loop
       let counter = 0;
 
       for (let value of values) {
         if (number >= value) {
-          symbol += symbols[counter]
+          // Add symbol to result, subtract the symbol's value from number, reset the counter, restart loop.
+          symbolResult += symbols[counter]
           number -= value
           counter = 0;
           break
         } else if (number === (value - values[counter+2])) {
-          symbol += (symbols[counter+2] + symbols[counter])
+          // Check if symbol should be preceded by second smaller symbol
+          symbolResult += (symbols[counter+2] + symbols[counter])
           number -= (value - values[counter+2])
           counter = 0;
           break
         } else if (number === (value - values[counter+1])) {
-          symbol += (symbols[counter+1] + symbols[counter])
+          // Check if symbol should be preceded by next smaller symbol
+          symbolResult += (symbols[counter+1] + symbols[counter])
           number -= (value - values[counter+1])
           counter = 0;
           break
+        } else {
+          // Increment counter if current symbol not needed
+          counter++
         }
-        counter++
       }
     }
-    return symbol;
+    return symbolResult;
 
   }
 }
